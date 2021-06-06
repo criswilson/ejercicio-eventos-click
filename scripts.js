4dfcf7f8fb6648d46154eee9cf9692f4e7df5047
cristian <cristian.wilson3@gmail.com>
2021-06-06 19:46:03 -0300

    ejercicio: agregar y eliminar elementos del DOM

--- scripts.js
+++ scripts.js
@@ -1,5 +1,20 @@
-function inputValue(e) {
-  if (e.key == "+") {
-    console.log(e.target.value);
+function addElementToDOM(type, content, parentId) {
+  const parent = document.getElementById(parentId);
+  const elem = document.createElement(type);
+  const text = document.createTextNode(content);
+
+  elem.appendChild(text);
+  parent.appendChild(elem);
+}
+
+function addItem(e) {
+  const taskValue = document.getElementById("typeUsername").value;
+  if (e.key == "+" || e.keyCode == "13") {
+    e.preventDefault();
+    addElementToDOM("li", taskValue, "list");
+    document.getElementById("typeUsername").value = "";
   }
 }
+
+const list = document.querySelectorAll("#list");
+console.log(list);
